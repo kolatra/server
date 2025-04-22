f5379d0258cd65fde1c52c067fbc2fab17da1a4a
tyler <loukstyler@gmail.com>
2025-04-22 11:43:12 -0600

    fix users

--- flake.nix
+++ flake.nix
@@ -12,17 +12,17 @@
     nixosConfigurations = {
         titan = nixpkgs.lib.nixosSystem {
             system = "x86_64-linux";
-	    specialArgs = {inherit inputs outputs;};
+            specialArgs = {inherit inputs outputs;};
             modules = [./cfg/configuration.nix];
         };
     };
 
     homeConfigurations = {
-    	"host@titan" = home-manager.lib.homeManagerConfiguration {
-	    pkgs = nixpkgs.legacyPackages.x86_64-linux;
-	    extraSpecialArgs = {inherit inputs outputs;};
-	    modules = [./home-manager/home.nix];
-	};
+        "host@titan" = home-manager.lib.homeManagerConfiguration {
+            pkgs = nixpkgs.legacyPackages.x86_64-linux;
+            extraSpecialArgs = {inherit inputs outputs;};
+            modules = [./home-manager/home.nix];
+        };
     };
   };
 }
