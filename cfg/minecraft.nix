19f0ec7c8c1f355da79462b7402ad9a15e560a5b
tyler <loukstyler@gmail.com>
2025-04-22 18:58:03 -0600

    update

--- cfg/minecraft.nix
+++ cfg/minecraft.nix
@@ -1,30 +1,7 @@
 { config, pkgs, inputs, ...}:
 
 let 
-  inherit (lib) concatStringsSep;
   jre21 = pkgs.temurin-bin-21;
-
-  # "Borrowed" from AllTheMods Discord
-  jvmOpts = concatStringsSep " " [
-    "-XX:+UseG1GC"
-    "-XX:+ParallelRefProcEnabled"
-    "-XX:MaxGCPauseMillis=200"
-    "-XX:+UnlockExperimentalVMOptions"
-    "-XX:+DisableExplicitGC"
-    "-XX:+AlwaysPreTouch"
-    "-XX:G1NewSizePercent=40"
-    "-XX:G1MaxNewSizePercent=50"
-    "-XX:G1HeapRegionSize=16M"
-    "-XX:G1ReservePercent=15"
-    "-XX:G1HeapWastePercent=5"
-    "-XX:G1MixedGCCountTarget=4"
-    "-XX:InitiatingHeapOccupancyPercent=20"
-    "-XX:G1MixedGCLiveThresholdPercent=90"
-    "-XX:G1RSetUpdatingPauseTimePercent=5"
-    "-XX:SurvivorRatio=32"
-    "-XX:+PerfDisableSharedMem"
-    "-XX:MaxTenuringThreshold=1"
-  ];
 
   defaults = {
     white-list = true;
