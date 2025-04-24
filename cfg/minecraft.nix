34043b18eb21839b67c71ea899179a369168f0c0
tyler <loukstyler@gmail.com>
2025-04-24 13:12:05 -0600

    formatting

--- cfg/minecraft.nix
+++ cfg/minecraft.nix
@@ -15,15 +15,16 @@ in {
         jvmInitialAllocation = "8G";
         jvmPackage = jre21;
 
+        rsyncSSHKeys = ["qwerty"]; # workaround for mkaito/nixos-modded-minecraft-servers/issues/6
+
         serverConfig = {
-	    server-port = 25565;
+            server-port = 25565;
             motd = "GregTech: New Horizons 2.7.2";
-	    white-list = true;
-	    spawn-protection = 0;
-	    max-tick-time = 5 * 60 * 1000;
-	    allow-flight = true;
+            white-list = true;
+            spawn-protection = 0;
+            max-tick-time = 5 * 60 * 1000;
+            allow-flight = true;
         };
-	rsyncSSHKeys = ["qwerty"]; # workaround for mkaito/nixos-modded-minecraft-servers/issues/6
       };
     };
   };
