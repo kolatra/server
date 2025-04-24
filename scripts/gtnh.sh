9f10f817621484c937319310d2fe74ed91f54a14
tyler <loukstyler@gmail.com>
2025-04-24 15:55:47 -0600

    make install script not quiet

--- scripts/gtnh.sh
+++ scripts/gtnh.sh
@@ -2,7 +2,8 @@
 
 URL="https://downloads.gtnewhorizons.com/ServerPacks/GT_New_Horizons_2.7.4_Server_Java_17-21.zip"
 
-wget -qO /tmp/archive.zip "$URL"
-unzip -q /tmp/archive.zip -d ./gt
+wget -O /tmp/archive.zip "$URL"
+mkdir ./gt
+unzip /tmp/archive.zip -d ./gt
 mv ./gt/startserver-java9.sh ./gt/start.sh
 sudo mv ./gt/* /var/lib/mc-gtnh
