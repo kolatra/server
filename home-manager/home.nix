ef7d478fbcd431e7dcad344ff97f0dbd41c26363
tyler <loukstyler@gmail.com>
2025-04-24 02:25:11 -0600

    add ohmyzsh

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -3,27 +3,43 @@
     config,
     pkgs,
     ...
-}: {
+}: let user = "tyler"; in {
     imports = [
 
     ];
 
     home = {
-        username = "tyler";
+        username = "${user}";
         homeDirectory = "/home/tyler";
     };
 
     programs.neovim.enable = true;
     home.packages = with pkgs; [
         zsh
+        oh-my-zsh
     ];
 
+    programs.zsh = {
+        enable = true;
+        enableCompletions = true;
+        autosuggestions.enable = true;
+        syntaxHighlighting.enable = true;
+
+        oh-my-zsh = {
+            enable = true;
+            theme = "robbyrussell";
+            plugins = [
+                "sudo"
+            ];
+        };
+    };
+
     home.file.".zshrc" = {
-        source = config.lib.file.mkOutOfStoreSymlink "/home/tyler/config/dotfiles/.zshrc";
+        source = config.lib.file.mkOutOfStoreSymlink "/home/${user}/config/dotfiles/.zshrc";
     };
 
     home.file.".config/nvim" = {
-        source = config.lib.file.mkOutOfStoreSymlink "/home/tyler/config/dotfiles/kickstart.nvim";
+        source = config.lib.file.mkOutOfStoreSymlink "/home/${user}/config/dotfiles/kickstart.nvim";
     };
 
     # not sure yet if I need this
@@ -39,7 +55,7 @@
 
     programs.git = {
         enable = true;
-        userName = "tyler";
+        userName = "${user}";
         userEmail = "3821892+kolatra@users.noreply.github.com";
     };
 
