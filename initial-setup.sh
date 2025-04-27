fe59833cd3d028c05de4745562646dc3839660c6
tyler <loukstyler@gmail.com>
2025-04-26 21:39:41 -0600

    make each instance generate its own hardware config

--- initial-setup.sh
+++ initial-setup.sh
@@ -3,7 +3,7 @@
 # Remember to add SSH keys to the machine
 # I won't add them to git :)
 
-rm ./cfg/hardware-configuration.nix
+rm -f ./cfg/hardware-configuration.nix
 sudo nixos-generate-config
 sudo mv /etc/nixos/hardware-configuration.nix ~/config/cfg/
 sudo chown tyler:users ./cfg/hardware-configuration.nix
@@ -13,4 +13,4 @@ sudo nixos-rebuild switch --flake .#titan
 chmod +x ./scripts/gtnh.sh
 ./scripts/gtnh.sh
 
-sudo nixos-rebuild switch --flake .#titan
+# sudo systemctl start gtnh-mc.service
