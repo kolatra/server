ecc0a929677f4d7625ec891a4517875ed3ddfe98
tyler <3821892+kolatra@users.noreply.github.com>
2025-05-04 23:37:14 -0600

    a

--- hosts/tethys/configuration.nix
+++ hosts/tethys/configuration.nix
@@ -98,6 +98,14 @@
      clang
   ];
 
+  users.users.tyler.shell = pkgs.zsh;
+
+  programs.zsh.enable = true;
+  programs.zsh.autosuggestions.enable = true;
+  programs.zsh.ohMyZsh = {
+      enable = true;
+  };
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
