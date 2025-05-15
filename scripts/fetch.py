1d53e7b8402c66aad667e15f373d539b81c45278
Tyler <3821892+kolatra@users.noreply.github.com>
2025-05-14 18:10:17 -0600

    close #1

--- scripts/fetch.py
+++ scripts/fetch.py
@@ -6,15 +6,15 @@ def main():
     with open(filename, 'r') as file:
         lines = [line.rstrip() for line in file]
 
-        index = random.randint(0, len(lines))
+        index = random.randint(1, len(lines) - 2)
 
         line = lines[index]
 
-        parts = line.split()
+        parts = line.split('"')
         logo = parts[1]
 
         print(f'Using {logo}')
-        os.system(f'fastfetch -l {logo}')
+        os.system(f'fastfetch -l "{logo}"')
 
 if __name__ == "__main__":
     main()
